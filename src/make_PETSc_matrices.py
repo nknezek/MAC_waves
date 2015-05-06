3ab859462f157e634a6beb7b1822d65f2c7fff99
nknezek <nknezek@gmail.com>
2015-05-06 11:55:56 -0700

    Updating SLEPc Code

--- src/make_PETSc_matrices.py
+++ src/make_PETSc_matrices.py
@@ -1,10 +1,14 @@
+#! /usr/bin/env python
+
 import numpy as np
 from numpy import sin
 from numpy import cos
 from datetime import datetime
-import os
+import sys
+from optparse import OptionParser
 import macmodel_condcore as mac
 import macloglib as mlog
+
 
 # mode to simulate
 m = 0
@@ -14,8 +18,7 @@ m_values = range(m_min, m_max+1)
 
 # Size of grid
 Nk = 20
-Nl = 200
-
+Nl = 201
 
 # Define Physical Constants
 R = 3480e3  # Outer core radius in m
@@ -26,13 +29,32 @@ nu = 1.  # momentum diffusivity in m^2/s
 eta = 2.  # magnetic diffusivity in m^2/s
 mu_0 = 4.*np.pi*10.**-7  # vacuum permeability in (kg*m/(A^2s^2))
 g = 10.  # Gravity in m/s^2
-T1 = 65.0*365.25*24*3600  # Period of first mode in seconds
+T1 = 72.0*365.25*24*3600  # Period of first mode in seconds
 l1 = 2*np.pi/(T1)  # non-dimensional frequency of first mode in rad/s
-delta_C = np.sqrt(2*nu/(2*np.pi/T1))  # Core magnetic skin depth for first mode in m
+delta_C = np.sqrt(2*eta/(l1))  # Core magnetic skin depth in m
+
+#==============================================================================
+# Allow usage of command line arguments 
+#==============================================================================
+parser = OptionParser()
+parser.add_option('-k','--Nk', type='int', dest='Nk', help="set Nk in model",
+                    default=Nk)
+parser.add_option('-l','--Nl', type='int', dest='Nl', help="set Nl in model",
+                    default=Nl)
+parser.add_option('-n','--nu', type='float', dest='nu',
+                    help="set nu in model", default=nu)
+parser.add_option('-d','--delta_C', type='float', dest='delta_C',
+                    help="set delta_C in model", default=delta_C)
+(options, args) = parser.parse_args()
+Nk = options.Nk
+Nl = options.Nl
+nu = options.nu
+delta_C = options.delta_C
 
 #===============================================================================
 #%% Set up model
 #===============================================================================
+
 model_parameters = {'Nk': Nk, 'Nl': Nl, 'm_values': m_values}
 physical_constants = {'R': R, 'Omega': Omega, 'rho': rho, 'h': h, 'nu': nu,
                       'eta': eta, 'mu_0': mu_0, 'delta_C': delta_C, 'g': g,
@@ -64,7 +86,7 @@ model.omega_g = omega_g0
 dir_name = ('../data/k'+str(Nk) + '_l' + str(Nl) +
             '_nu{0:.2e}_dC={1:.2e}/'.format(nu, delta_C))
 mlog.ensure_dir(dir_name)
-filemodel = 'model_k'+str(Nk)+'_l'+str(Nl)+'.p'
+filemodel = 'model.p'
 model.save_model(dir_name + filemodel)
 
 #===============================================================================
@@ -76,8 +98,8 @@ model.make_M(m)
 #===============================================================================
 #%% Save PETSc Matrices
 #===============================================================================
-fileA = 'A_k'+str(Nk)+'_l'+str(Nl)+'.dat'
-fileM = 'M_k'+str(Nk)+'_l'+str(Nl)+'.dat'
+fileA = 'A.dat'
+fileM = 'M.dat'
 
 model.save_mat_PETSc(dir_name+fileA, model.A.toPETSc())
 model.save_mat_PETSc(dir_name+fileM, model.M.toPETSc())
--- src/solve_SLEPc.py
+++ src/solve_SLEPc.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Mon May  4 16:44:50 2015
@@ -11,33 +12,55 @@ import macplotlib as mplt
 import sys
 import slepc4py
 import cPickle as pkl
+from optparse import OptionParser
 slepc4py.init(sys.argv)
 from petsc4py import PETSc
 from slepc4py import SLEPc
 opts = PETSc.Options()
 
+Target = 5.5e-5j
+nev = 10
+data_dir = '../data/k20_l201_nu1.00e+00_dC=3.80e+04/'
+out_dir = '../output/SLEPc_run_{0}_{1}/'.format(datetime.today().date(),
+                                                 datetime.today().time())    
 
-dir_name = '../output/SLEPc_run_{0}_{1}/'.format(datetime.today().date(),
-                                                 datetime.today().time())
+# Allow usage of command line arguments 
+parser = OptionParser()
+parser.add_option('-t','--Target', type='complex', dest='Target',
+                  help="set Target in model", default=Target)
+parser.add_option('-n','--nev', type='int', dest='nev',
+                  help="set nev in model", default=nev)
+parser.add_option('--data_dir', type='string', dest='data_dir',
+                  help="set data_dir in model", default=data_dir)
+parser.add_option('--out_dir', type='string', dest='out_dir',
+                  help="set data_dir in model", default=out_dir)
+(options, args) = parser.parse_args()
+Target = options.Target
+nev = options.nev
+data_dir = options.data_dir
+out_dir = options.out_dir
 
-logger = mlog.setup_custom_logger(dir_name=dir_name, filename='SLEPc.log')
+logger = mlog.setup_custom_logger(dir_name=out_dir, filename='SLEPc.log')
 
 #==============================================================================
-#%%  Load Matrices from files
+#%%  Load Matrices and model from files
 #==============================================================================
-fileA = 'A_k20_l201.dat'
-fileB = 'M_k20_l201.dat'
+filemodel = 'model.p'
+fileA = data_dir + 'A.dat'
+fileB = data_dir + 'M.dat'
+
 viewer = PETSc.Viewer().createBinary(fileA, 'r')
 A = PETSc.Mat().load(viewer)
 viewer = PETSc.Viewer().createBinary(fileB, 'r')
 B = PETSc.Mat().load(viewer)
-logger.info('matrices loaded into memory from ' + fileA + ' and ' + fileB)
+model = pkl.load(open(data_dir+filemodel,'rb'))
+logger.info('matrices and model loaded into memory from ' + data_dir)
 
 #==============================================================================
 #%% Set up SLEPc Solver
 #==============================================================================
-Target = 4.4e-5j
-nev = 10
+
+
 EPS = SLEPc.EPS().create()
 EPS.setDimensions(nev, PETSc.DECIDE)
 EPS.setOperators(A, B)
@@ -61,12 +84,28 @@ logger.info('problem solved')
 savefile = 'data.p'
 conv = EPS.getConverged()
 logger.info('{0} eigenvalues converged'.format(conv))
-vs, ws = PETSc.Mat.getVecs(A)
+
 vals = []
 vecs = []
 for ind in range(conv):
-    vals.append(EPS.getEigenpair(ind, ws))
+    vs, ws = PETSc.Mat.getVecs(A)
+    v = EPS.getEigenpair(ind, ws)
+    vals.append(v)
     vecs.append(ws.getArray())
-pkl.dump({'vals': vals, 'vecs': vecs},open(dir_name + savefile, 'wb'))
-logger.info('vals and vecs saved to '+dir_name + savefile)
+pkl.dump({'vals': vals, 'vecs': vecs},open(out_dir + savefile, 'wb'))
+logger.info('vals and vecs saved to ' + out_dir + savefile)
+
+#==============================================================================
+#%% Plot Found Vectors
+#==============================================================================
+for ind in range(conv):
+    title = 'SLEPc Run Nk={0}, Nl={1}, ind={2}, val={3:.2e}'.format(model.Nk,
+                                                                    model.Nl,
+                                                                    ind,
+                                                                    vals[ind])
+    mplt.plot_pcolor_rth(model, vals[ind], vecs[ind], dir_name=out_dir,
+                         title=title)
+    logger.info('plotted ind={0}'.format(ind))
+
 logger.info('run complete')
+
