4eb92565ffd7a4fb79044c5f0e9d9a42279c7657
Nick Knezek <nknezek@mcc-411-6.geo.berkeley.edu>
2016-02-19 10:59:26 -0800

    test

--- config/cfg_braginsky_MAC_match.py
+++ config/cfg_braginsky_MAC_match.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 import numpy as np
+#import macmodel_condcore2 as mac
+#import macmodel_noCC_sympBC as mac
 import macmodel_braginsky as mac
 import macloglib as mlog
 
@@ -7,21 +9,21 @@ import macloglib as mlog
 m = [0]
 
 # Size of grid
-Nk = 30 # Radial cells
-Nl = 601 # Latitudinal cells
-h = [80e3]  # layer thickness in (m)
+Nk = 20 # Radial cells
+Nl = 200 # Latitudinal cells
 
 # Define Physical Constants
 R = 3480e3  # Outer core radius in (m)
+h = [80e3]  # layer thickness in (m)
 Omega = 2*np.pi/(23.9345*3600.0)  # rotation rate in (rad/s)
 rho = 1.e4   # density in (kg/m^3)
-nu = 1.e-6  # momentum diffusivity in (m^2/s)
-eta = 2.  # magnetic diffusivity in (m^2/s)
+nu = [1e-5]   # momentum diffusivity in (m^2/s)
 nu_th = 0.0
+eta = 2.0  # magnetic diffusivity in (m^2/s)
 eta_th = 0.0
 mu_0 = 4.*np.pi*10.**-7  # vacuum permeability in (kg*m/(A^2s^2))
 g = 10.  # Gravity in m/s^2
-dCyr = 71.
+dCyr = [71.]
 
 # background magnetic field in (Tesla)
 # chocies: dipole, dipole_Br, abs_dipole, abs_dipole_Br, constant_Br, set, Br_sinfunc
@@ -42,10 +44,16 @@ Uphi = np.ones((Nk+2, Nl+2))*0.0
 # choices: constant, linear
 buoy_type = 'constant'
 buoy_ratio =  [2]
+#omega_g0 = buoy_ratio*Omega  # Maximum buoyancy frequency in (rad/s)
+#drho_dr_0 = -omega_g0**2*rho/g  # Maximum density gradient
+# linear distribution from core fluid to mantle
+#drho_dr = (np.ones((Nk+2, Nl+2)).T*np.linspace(0, drho_dr_0, Nk+2)).T
+# constant distribution
+#drho_dr = np.ones((Nk+2, Nl+2))*drho_dr_0
 
 # model parameters
 model_variables = ('ur', 'uth', 'uph', 'br', 'bth', 'bph', 'p', 'r_disp')
 boundary_variables = ('ur', 'uth', 'uph', 'br', 'bth', 'bph', 'p')
-dir_suf = '_dipoleBr_n1e-6_ep1e-5_rlorentz'
-ep = 1e-5
+dir_suf = '_reducedB_ep1e-3'
 
+ep = 1e-3
